b0eb8e0b975569c859825c3e513886d48e3c045b
Servan42 <charlot.servan@gmail.com>
2023-10-03 23:44:24 +0200

    wip: Genetic algorithm that does not help

---
+++ Peg_SolitaireTDD/api/GeneticAlgorithmService.cs
@@ -0,0 +1,98 @@
+﻿using Peg_SolitaireTDD.Model;
+using Peg_SolitaireTDD.spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peg_SolitaireTDD.api
+{
+    public class GeneticAlgorithmService
+    {
+        public List<ReplayStep> Run()
+        {
+            int populationSize = 1000;
+            int numberOfLocusToKeep = 20;
+            int maxGenerationNumber = 1000;
+            int genCount = 0;
+
+            var population = GenerateBasePopulation(populationSize);
+            while (genCount < maxGenerationNumber)
+            {
+                population = SelectBestLocus(numberOfLocusToKeep, population);
+                Console.WriteLine(population[0].Score);
+                population = BreedAndMutate(population, populationSize, 0.01);
+                genCount++;
+            }
+            return null;
+        }
+
+        public List<ReplayGame> GenerateBasePopulation(int populationSize)
+        {
+            List<ReplayGame> returnPopulation = new();
+            for (int a = 0; a < populationSize; a++)
+            {
+                ReplayGame gameReplay = new();
+                gameReplay.replaySteps.Add(new ReplayStep((3, 1), (3, 3, GameServiceHelper.MOVE_TOWARDS_J)));
+                for (int b = 0; b < GameServiceHelper.MAX_MOVES_IN_A_GAME - 1; b++)
+                {
+                    var initalPosition = GameServiceHelper.GenerateRandomValidInitialPosition();
+                    var destination = GameServiceHelper.GenerateRandomValidDestinationPositionFromInitialPosition(initalPosition);
+                    gameReplay.replaySteps.Add(new ReplayStep(initalPosition, destination));
+                }
+                returnPopulation.Add(gameReplay);
+            }
+            return returnPopulation;
+        }
+
+        public List<ReplayGame> SelectBestLocus(int numberOfLocusToKeep, List<ReplayGame> population)
+        {
+            ReplayService replayService = new((3, 3));
+
+            population.ForEach(l => l.Score = replayService.ComputeReplayScore(l.replaySteps));
+
+            return population.OrderBy(l => l.Score)
+                .Take(numberOfLocusToKeep)
+                .ToList();
+        }
+
+        public ReplayGame Breed(ReplayGame parent1, ReplayGame parent2)
+        {
+            ReplayGame child = new();
+            for (int i = 0; i < GameServiceHelper.MAX_MOVES_IN_A_GAME; i = i + 2)
+            {
+                child.replaySteps.Add(parent1.replaySteps[i]);
+                child.replaySteps.Add(parent2.replaySteps[i + 1]);
+            }
+            return child;
+        }
+
+        public ReplayGame Mutate(ReplayGame child)
+        {
+            child.replaySteps.ForEach(x =>
+            {
+                var initalPosition = GameServiceHelper.GenerateRandomValidInitialPosition();
+                var destination = GameServiceHelper.GenerateRandomValidDestinationPositionFromInitialPosition(initalPosition);
+                x.BallInitialPosition = initalPosition;
+                x.BallDestination = destination;
+            });
+            return child;
+        }
+
+        public List<ReplayGame> BreedAndMutate(List<ReplayGame> population, int populationSize, double mutationProbability)
+        {
+            List<ReplayGame> replayGames = new();
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                var child = Breed(population[StaticRandom.Rand(0, population.Count)], population[StaticRandom.Rand(0, population.Count)]);
+                if ((mutationProbability * 100) > StaticRandom.Rand(0, 101)) child = Mutate(child);
+                replayGames.Add(child);
+            }
+
+            return replayGames;
+        }
+    }
+}
---
+++ Peg_SolitaireTDDTest/GeneticAlgorithmServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Peg_SolitaireTDD;
+using Peg_SolitaireTDD.api;
+using Peg_SolitaireTDD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peg_SolitaireTDDTest
+{
+    public class GeneticAlgorithmServiceTests
+    {
+        // Establish a population
+        // Select the n best locus
+        // Breed them to full pop
+        // Mutate some of them
+        // Loop until max generation number or satisfying result
+
+        GeneticAlgorithmService _geneticAlgorithmService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _geneticAlgorithmService = new GeneticAlgorithmService();
+        }
+
+        [Test]
+        public void Should_init_a_start_population()
+        {
+            // Given
+            List<ReplayGame> population;
+            int populationSize = 100;
+
+            // When
+            population = _geneticAlgorithmService.GenerateBasePopulation(populationSize);
+
+            // Then
+            Assert.That(population.Count, Is.EqualTo(populationSize), "Population size is invalid");
+            Assert.True(population.All(l => l.replaySteps.Count == 36), "ReplayStep list size is invalid");
+        }
+
+        [Test]
+        public void Should_select_the_best_locus()
+        {
+            // Given
+            List<ReplayGame> bestLocusList;
+            int populationSize = 100;
+            int numberOfLocusToKeep = 20;
+            List<ReplayGame> population = _geneticAlgorithmService.GenerateBasePopulation(populationSize);
+
+            // When
+            bestLocusList = _geneticAlgorithmService.SelectBestLocus(numberOfLocusToKeep, population);
+
+            // Then
+            Assert.That(bestLocusList.Count, Is.EqualTo(numberOfLocusToKeep), "Number of kept locus is invalid");
+        }
+    }
+}
